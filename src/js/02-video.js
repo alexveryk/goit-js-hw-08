2e4ec2301eaec320cc56c0c2fc3d29bc86194215
alexveryk <veryk.av@gmail.com>
2022-12-18 23:00:43 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const currentTime = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+  //   console.log(data);
+};
+
+player.on('timeupdate', currentTime);
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
