059fd95defaeeb3dc94709eb9a3c513759962aae
alexveryk <veryk.av@gmail.com>
2022-12-19 16:14:32 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+feedbackForm.addEventListener('input', throttle(inputType, 500));
+feedbackForm.addEventListener('submit', onSubmit);
+
+const form = {};
+const FEDBACK_FORM = 'feedback-form-state';
+
+function inputType(event) {
+  form[event.target.name] = event.target.value;
+  localStorage.setItem(FEDBACK_FORM, JSON.stringify(form));
+}
+
+const getForm = JSON.parse(localStorage.getItem(FEDBACK_FORM));
+
+let email = getForm.email || '';
+let message = getForm.message || '';
+
+(function () {
+  feedbackForm['email'].value = email;
+  feedbackForm['message'].value = message;
+})();
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  evt.target.reset();
+
+  console.log(getForm);
+
+  localStorage.clear();
+}
