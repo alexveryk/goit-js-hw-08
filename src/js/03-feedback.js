0f35a48cd1612f96566662ccb3b7a379877db2f2
alexveryk <veryk.av@gmail.com>
2022-12-21 00:50:48 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,14 +1,26 @@
 import throttle from 'lodash.throttle';
 
-const form = {};
 const FEDBACK_FORM = 'feedback-form-state';
 
 const feedbackForm = document.querySelector('.feedback-form');
+const subbmit = document.querySelector('button[type="submit"]');
 feedbackForm.addEventListener('input', throttle(inputType, 500));
 feedbackForm.addEventListener('submit', onSubmit);
+subbmit.disabled = true;
+savinFormData();
 
-function inputType(event) {
-  form[event.target.name] = event.target.value;
+function inputType() {
+  const form = {
+    email: feedbackForm.email.value,
+    message: feedbackForm.message.value,
+  };
+
+  if (feedbackForm.email.value && feedbackForm.message.value) {
+    subbmit.disabled = false;
+  } else {
+    subbmit.disabled = true;
+  }
+
   localStorage.setItem(FEDBACK_FORM, JSON.stringify(form));
 }
 
@@ -16,17 +28,22 @@ function savinFormData() {
   const getForm = JSON.parse(localStorage.getItem(FEDBACK_FORM));
 
   if (getForm) {
-    feedbackForm['email'].value = getForm.email;
-    feedbackForm['message'].value = getForm.message;
+    feedbackForm['email'].value = getForm.email || '';
+    feedbackForm['message'].value = getForm.message || '';
+  }
+
+  if (feedbackForm.email.value && feedbackForm.message.value) {
+    subbmit.disabled = false;
   }
 }
-savinFormData();
 
 function onSubmit(evt) {
   evt.preventDefault();
 
-  console.log(form);
+  const getForm = JSON.parse(localStorage.getItem(FEDBACK_FORM)) || {};
+  console.log(getForm);
 
   evt.target.reset();
   localStorage.removeItem(FEDBACK_FORM);
+  subbmit.disabled = true;
 }
